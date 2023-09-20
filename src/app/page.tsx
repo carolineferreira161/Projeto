94909010be1809a3dfdbad9ca5973ec54f7f5af3
caroline <carolfr3011@gmail.com>
2023-09-19 22:44:01 -0300

    3 atualização

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ export default function Home() {
           <a href="https://www.linkedin.com/in/caroline-ferreira-ribeiro-5bba08192/">
             <img src="/t.png" width={30} className="image"/>
           </a>
-          <h1 className="font-bold">Seu nome</h1>
+          <h1 className="font-bold">Matheus Gonçalves</h1>
         </nav>
 
         <nav className='navbar'>
-          <a href="" className="active">portifólio</a>
-          <a href="https://www.linkedin.com/in/caroline-ferreira-ribeiro-5bba08192/">contato</a>
-          <a href="https://www.linkedin.com/in/caroline-ferreira-ribeiro-5bba08192/">
+          <a href="" className="active">portfólio</a>
+          <a href="https://www.linkedin.com/in/g0nc/overlay/contact-info/">contato</a>
+          <a href="https://www.linkedin.com/in/g0nc/overlay/contact-info/">
             <img
               src="linkedin_branco.png"
               title="Linkedin"
@@ -31,7 +31,16 @@ export default function Home() {
           </a>
         </nav>
       </header>
-
+     
+    <section className='home' id='home'>
+      <div className='home-content'>
+       <h1>Olá, sou Matheus Gonçalves</h1>
+        <div className='text-animate'>
+          <h3>Engenheiro de Vendas</h3>
+        </div> 
+        <p> Regulamento geral sobre a proteção de dados (RGPD) Tecnologia da informação Gestão de vulnerabilidade</p>
+      </div>
+    </section>
 
 
       <footer>
